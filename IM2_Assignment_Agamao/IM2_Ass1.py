d63a8652e77c0da03b921e419dc180a1b408478a
Aaron <agamaoaaronjohn21@gmail.com>
2025-09-03 22:46:02 +0800

    Upload IM2 assignment and practice folder and files

---
+++ IM2_Assignment_Agamao/IM2_Ass1.py
@@ -0,0 +1,20 @@
+while True:
+    rows = int(input('Enter Row: '))
+    col = int(input('Enter Col: '))
+
+    # check if valid first
+    if rows <= 0 or col <= 0:
+        print('Invalid Input. Loop is stopped.')
+        break
+
+    search = int(input('Search a number: '))
+
+    # print matrix with highlighted search
+    for x in range(1, rows + 1):
+        for y in range(1, col + 1):
+            value = x * y
+            if search == value:
+                print(f'[{value}]', end=" ")
+            else:
+                print(value, end=" ")
+        print()
